7c7d849bd0c101645b8c62ffa31b471b1ee8aa09
Your Name <diego-vespa2020@outlook.com.br>
2020-07-28 16:59:54 -0300

    arrumando nav

--- src/index.js
+++ src/index.js
@@ -16,8 +16,8 @@ ReactDOM.render(
   <BrowserRouter>
     <Switch>
       <Route path="/" component ={Home} exact/>
-      <Route path="/cadastro/video" component ={CadastroVideo} />
-      <Route path="/cadastro/categoria" component ={CadastroCategoria} />
+      <Route path="/cadastro/Video" component ={CadastroVideo} />
+      <Route path="/cadastro/Categoria" component ={CadastroCategoria} />
       <Route component ={Pag404}/>
     </Switch>
   </BrowserRouter>,
