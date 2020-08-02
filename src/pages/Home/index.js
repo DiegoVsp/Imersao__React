431e545845565ef3ab6f1ba857a9d6548939c15d
Your Name <diego-vespa2020@outlook.com.br>
2020-08-02 11:11:26 -0300

    cadastro video bd funcionando

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from 'react';
-// import dadosIniciais from '../../data/dados_iniciais.json';
 import BannerMain from '../../components/BannerMain';
 import Carousel from '../../components/Carousel';
 import PageDefault from '../../components/PageDefault';
@@ -11,7 +10,7 @@ function Home() {
   useEffect(() => {
     categoriasRepository.getAllWithVideos()
       .then((categoriasComVideos) => {
-        console.log(categoriasComVideos[0].videos[0]);
+        console.log(categoriasComVideos);
         setDadosIniciais(categoriasComVideos);
       })
       .catch((err) => {
@@ -22,9 +21,12 @@ function Home() {
   return (
     <PageDefault paddingAll={0}>
       {dadosIniciais.length === 0 && (<div>Loading...</div>)}
-      {dadosIniciais.length >= 1 && (
-        
-            <>
+
+
+      {dadosIniciais.map((categoria, indice) => {
+        if (indice === 0) {
+          return (
+            <div key={categoria.id}>
               <BannerMain
                 videoTitle={dadosIniciais[0].videos[0].titulo}
                 url={dadosIniciais[0].videos[0].url}
@@ -34,8 +36,16 @@ function Home() {
                 ignoreFirstVideo
                 category={dadosIniciais[0]}
               />
-            </>
-      )}
+            </div>
+          );
+        }
+        return (
+          <Carousel
+            key={categoria.id}
+            category={categoria}
+          />
+        );
+      })}
 
       {/* <BannerMain
         videoTitle={dadosIniciais.categorias[0].videos[0].titulo}
--- src/pages/cadastro/Video/index.js
+++ src/pages/cadastro/Video/index.js
@@ -1,13 +1,62 @@
 import React from 'react';
-import {Link} from 'react-router-dom';
+import { Link, useHistory} from 'react-router-dom';
 
 import PageDefault from '../../../components/PageDefault';
-
+import useForm from '../../../hooks/useForm';
+import FormField from '../../../components/FormField';
+import Button from '../../../components/Button';
+import videosRepository from '../../../repositories/videos'
 
 function CadastroVideo() {
-  return(
+  const history = useHistory();
+  const { HandleChange, values } = useForm({
+    titulo:'Video Padrão',
+    url:'https://www.youtube.com/watch?v=BkEk7C14ONE',
+    categoria:'Criaturas'
+  });
+
+  return (
     <PageDefault>
-     <h1>Cadastro de Video</h1>
+      <h1>Cadastro de Video</h1>
+
+      <form onSubmit={(event)=> {
+        event.preventDefault();
+        // alert('Video Cadastrado com sucesso!')
+      videosRepository.create({
+        titulo:values.titulo,
+        url: values.url,
+        categoriaId: 1
+      })
+      .then(()=> {
+        console.log('Cadastrou com sucesso')
+        history.push('/')
+      })
+
+        history.push('/')
+      }}>
+        <FormField
+          label="Título do Vídeo"
+          name="titulo"
+          value={values.titulo}
+          onChange={HandleChange}
+        />
+        <FormField
+          label="URL"
+          name="url"
+          value={values.url}
+          onChange={HandleChange}
+        />
+        <FormField
+          label="Categoria"
+          name="categoria"
+          value={values.categoria}
+          onChange={HandleChange}
+        />
+
+        <Button type="submit">
+          Novo Vídeo
+        </Button>
+      </form>
 
       <Link to="/cadastro/categoria">
         Cadastrar Categoria
